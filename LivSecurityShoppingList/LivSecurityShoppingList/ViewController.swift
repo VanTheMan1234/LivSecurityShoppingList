bcf8853f7c578f9c8002826217127339ce8aced2
Vanoshan Ramdhani <vanoshan1234@gmail.com>
2022-03-01 10:05:27 +0200

    Added files via upload

---
+++ LivSecurityShoppingList/LivSecurityShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivSecurityShoppingList
+//
+//  Created by Vanoshan  Ramdhani on 2022/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
